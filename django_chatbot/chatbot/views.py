1898f864b02f18ace3a73cb6e5d133566ceaeb94
Haroon-Bhat <haroonbhat1994@gmail.com>
2025-11-26 06:57:39 +0530

    initial commit

--- django_chatbot/chatbot/views.py
+++ django_chatbot/chatbot/views.py
@@ -12,12 +12,7 @@ from transformers import pipeline
 
 
 
-#openai_api_key = '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'
-#openai.api_key = openai_api_key
-#set OPENAI_API_KEY=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
-#set OPENAI_API_KEY=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
-# client = OpenAI(api_key=openai_api_key)
-# def ask_openai(message):
+
 
 #     try: 
 #         response = client.chat.completions.create(
@@ -55,31 +50,10 @@ def ask_huggingface(message,user_id):
         return answer
     except Exception:
         return "Sorry, I couldn't process your message at the moment."
-    #try:
-    # chat_history = conversations.get(user_id, "")
-    # #input_text = chat_history + "\nUser: " + message + "\nBot:"
-    # input_text = message
-
-    # response = chatbot_pipeline(
-    #     input_text,
-    #     max_new_tokens=100,
-    #     pad_token_id=chatbot_pipeline.tokenizer.eos_token_id
-    # )
+   
 
         
-    # reply = response[0]['generated_text'].strip()
-    # # Update history
-    # conversations[user_id] = input_text + reply + "\n"
-    # return reply
-        # your chatbot generation code here
-        #response = ...  # generate response
-        # if not response:
-        #     return "Sorry, I couldn't generate a response."
-        # return response
-
-
-    # except Exception:
-    #     return "Sorry, something went wrong. Please try again."
+   
 
     
 
